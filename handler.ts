83db552decb0c24e6f7a620327910697816e308b
masaki.koide <ikasama0324@gmail.com>
2018-10-15 21:44:29 +0900

    feat: [WIP] ログイン処理

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
+import * as puppeteer from 'puppeteer'
 
-export const hello: Handler = (
+export const getRanking: Handler = (
   event: APIGatewayEvent,
   context: Context,
   cb: Callback
@@ -16,3 +17,28 @@ export const hello: Handler = (
 
   cb(null, response)
 }
+
+const crawl = async () => {
+  // FIXME:テスト用
+  const headless = !!process.env.NODE_ENV
+  const browser = await puppeteer.launch({ headless })
+  const page = await browser.newPage()
+  await page.goto('https://newspicks.com/')
+  const isLogined = await page.$('.self')
+  if (!isLogined) {
+    const loginButtonSelector = '.register-or-login-items > .login'
+    await page.waitForSelector(loginButtonSelector)
+    await page.click(loginButtonSelector)
+    const nameSelector = '[name=username]'
+    const passSelector = '[name=password]'
+    const buttonSelector = '.login-btn'
+    await page.type(nameSelector, process.env.USER_NAME)
+    await page.type(passSelector, process.env.PASSWORD)
+    await page.click(buttonSelector)
+    await page.waitForNavigation()
+  }
+  // TODO:Moment.js
+  await browser.close()
+}
+
+crawl()
