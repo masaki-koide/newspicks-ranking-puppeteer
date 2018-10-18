dc7c5f11c3ce929cf36a05e727f8467e06819c55
masaki.koide <ikasama0324@gmail.com>
2018-10-18 21:48:36 +0900

    refactor: クエリの組み立てを完全に別ファイルに分離、スクレイピング関数をLambdaに依存ない形に疎結合化

--- handler.ts
+++ handler.ts
@@ -1,21 +1,38 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
-import * as moment from 'moment'
 import * as puppeteer from 'puppeteer'
-import { Days, getDateQueryParams } from './helper'
+import { Days, getQueryStringForRanking } from './helper'
 
-const host = 'https://newspicks.com/'
+const domain = 'https://newspicks.com/'
 
-export const getRanking: Handler = (
+interface Article {
+  title: string
+  pickCount: number
+  date: string
+  url: string
+}
+
+export const getRanking: Handler = async (
   event: APIGatewayEvent,
-  context: Context,
+  _: Context,
   cb: Callback
 ) => {
+  const params = event.queryStringParameters
+  const range = Days[params.range]
+  let ranking: any
+  if (range) {
+    ranking = await scraping(Days[params.range])
+  } else {
+    console.log('error')
+    ranking = []
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message:
         'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event
+      input: event,
+      result: ranking
     })
   }
 
@@ -35,40 +52,34 @@ const login = async (page: puppeteer.Page) => {
   await page.waitForSelector(loginButton)
   await page.type(nameInput, process.env.USER_NAME)
   await page.type(passInput, process.env.PASSWORD)
-  await page.click(loginButton)
 
+  await page.click(loginButton)
   await page.waitForNavigation()
 }
 
-const crawl = async () => {
-  // FIXME:テスト用
-  const headless = !!process.env.NODE_ENV
+// FIXME:返り値の型
+const scraping = async (period: Days): Promise<any> => {
+  const headless = process.env.NODE_ENV === 'production'
+  console.log(`headless is ${headless}`)
   const browser = await puppeteer.launch({ headless })
   const page = await browser.newPage()
-  await page.goto(host)
+  await page.goto(domain)
+
   const isLogined = await page.$('.self')
   if (!isLogined) {
     await login(page)
   }
-  // TODO:periodのパラメータを受け取る
-  const { to, from } = getDateQueryParams(Days.day)
-  const queryParams = {
-    sort: 'picks',
-    q: '',
-    to,
-    from
-  }
-  const query = Object.keys(queryParams)
-    .map(key => `${key}=${queryParams[key]}`)
-    .join('&')
-  await page.goto(host + 'search?' + query)
+
+  const queryString = getQueryStringForRanking(period)
+  await page.goto(domain + queryString)
   await page.waitForSelector('.news-card-list')
-  const infos = await page.$$eval(
+
+  const ranking = await page.$$eval<Article[]>(
     '.news-card-list > .search-result-card',
     elements => {
       return elements.map(element => {
         const title = element.querySelector('.news-title').textContent
-        const pickCount = element.querySelector('.value').textContent
+        const pickCount = element.querySelector('.value').textContent as number
         const date = element.querySelector('.published').textContent
         const url = element.querySelector('.news-card > a').href
         return {
@@ -78,11 +89,18 @@ const crawl = async () => {
           url
         }
       })
-    },
-    host
+    }
   )
-  console.log(infos)
+
   await browser.close()
+  return ranking
+  // TODO:エラー処理
 }
 
-crawl()
+if (process.argv.length) {
+  scraping(Days[process.argv[2]])
+    .then(ranking => {
+      console.log(ranking)
+    })
+    .catch(error => console.log(error))
+}
--- helper.ts
+++ helper.ts
@@ -8,12 +8,14 @@ export enum Days {
   year = 365
 }
 
-export interface DateQueryParams {
+interface DateQueryParams {
   to: string
   from: string
 }
 
-export const getDateQueryParams = (period: Days): DateQueryParams => {
+export type QueryParams = DateQueryParams & { sort: string; q: string }
+
+const getDateQueryParams = (period: Days): DateQueryParams => {
   const dateFormat = 'YYYYMMDDHH'
   return {
     to: moment().format(dateFormat),
@@ -21,4 +23,23 @@ export const getDateQueryParams = (period: Days): DateQueryParams => {
       .subtract(period, 'days')
       .format(dateFormat)
   }
+}
+
+const getQueryParamsForRanking = (period: Days): QueryParams => {
+  const dateQueryParams = getDateQueryParams(period)
+  const sort = 'picks'
+  const q = ''
+  return {
+    ...dateQueryParams,
+    sort,
+    q
+  }
+}
+
+export const getQueryStringForRanking = (period: Days): string => {
+  const queryParams = getQueryParamsForRanking(period)
+  const query = Object.keys(queryParams)
+    .map(key => `${key}=${queryParams[key]}`)
+    .join('&')
+  return 'search?' + query
 }
